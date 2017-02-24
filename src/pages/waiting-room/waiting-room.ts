84e34b72ec5cacbd802091ec2c5541f76b5b0e5f
ScottHenry <scotthenry95@gmail.com>
2017-02-24 13:50:48 -0800

    Created waiting room page

--- src/pages/waiting-room/waiting-room.ts
+++ src/pages/waiting-room/waiting-room.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { AlertController, App, FabContainer, ItemSliding, List, ModalController, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 /*
   Generated class for the WaitingRoom page.
@@ -12,14 +12,43 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'waiting-room.html'
 })
 export class WaitingRoomPage {
+
+  @ViewChild('playerList', { read: List }) playerList: List;
+
   room: any;
+  shownPlayers: any = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.room = navParams.data;
   }
 
+  //Runs everytime a player joins the room
   ionViewDidLoad() {
     console.log('ionViewDidLoad WaitingRoomPage');
+    this.updatePlayerList();
+    this.initializeGame();
   }
+
+  //Updates the list of players in the room
+  //Test data until method to actually get real data
+  updatePlayerList(){
+    this.shownPlayers = [
+      {"name": "Scott" },
+      {"name": "JJ" },
+      {"name": "Thomas" }
+    ];
+
+    //this.shownRooms = data.shownRooms;
+    console.log('List of players updated!');
+  }
+
+  //Checks to see if enough players are there to start the game
+  initializeGame(){
+
+    //if (shownPlayers.count = 3){
+      //Send the player to the GamePage with either the room or player data
+      //this.navCtrl.push(GamePage, roomDate);
+    //}
+}
 
 }

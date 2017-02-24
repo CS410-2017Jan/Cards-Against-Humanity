c6664d0edd174ce9df4688ad94d37c537f7a694a
JJXN <jjxn@hotmail.ca>
2017-02-24 13:50:22 -0800

    very minor housekeeping. About to merge

--- src/data-classes/card-submission.ts
+++ src/data-classes/card-submission.ts
@@ -2,16 +2,16 @@
  * Created by Joshua Jackson on 23-Feb-17.
  */
 
-import { Card } from './card.ts';
+import { Card } from './card';
 
 // ======================================================================
 // This Class outlines the data structure of played Card
 // ======================================================================
 export class CardSubmission {
   username: string;
-  card: typeof Card;
+  card: Card;
 
-  constructor(username: string, card: typeof Card) {
+  constructor(username: string, card: Card) {
     this.username = username;
     this.card = card;
   }

7abb1d887c60410b0b6a60805a45c280c8c3338f
ScottHenry <scotthenry95@gmail.com>
2017-03-18 23:25:03 -0700

    Created a test upload photo button for #28 and #116

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  options;
+
   constructor(public navCtrl: NavController) {
+  }
+
+  static onSuccess() {
+    console.log("Camera cleanup success.");
+  }
+
+  static onFail(message) {
+    console.log('Failed because: ' + message);
+  }
+
+  clickButton(){
+    console.log('Button clicked!');
+    Camera.getPicture(
+      {quality: 25, destinationType: Camera.DestinationType.DATA_URL}).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (e) => {
+      console.log("error", e);
+      // Handle error
+    });
 
   }
 
